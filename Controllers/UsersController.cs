eca1d8d14a419ecd8ec8f185d2a3a661f0139c3a
rupeshyy <rupeshyy@gmail.com>
2025-02-04 23:02:02 +0530

    End of sction 10

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository,IMapper mapper) : BaseApiController
     {
         [HttpGet] // GET: api/Users
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -26,6 +27,24 @@ namespace API.Controllers
             var user = await userRepository.GetMemberAsync(username);
             if (user == null) return NotFound();
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(username == null) return BadRequest("No Username is found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if(user == null) return BadRequest("Could not find user");
+
+            mapper.Map(memberUpdateDto,user);
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("failed to update the user");
         }
     }
 }
